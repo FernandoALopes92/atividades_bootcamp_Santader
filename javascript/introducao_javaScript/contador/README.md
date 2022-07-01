fa6a289ad10ab7baf796dd3dee1b6d7c27c0b90b
FernandoALopes92 <fernando.lopes_92@hotmail.com>
2022-07-01 01:58:14 -0300

    deletando

--- javascript/introducao_javaScript/contador/README.md
+++
@@ -1,7 +0,0 @@
-## Contador
-
-- index - Exercício feito em aula
-- addEventListener
-- Desabilitar/Habilitar botão
-- Mudar cor do texto quando negativo
-
--- modulos/main.mjs
+++
@@ -1,5 +0,0 @@
-import { mostraCidade, mostraIdade , mostraHobby } from "./funcoes.mjs";
-
-console.log(mostraIdade('Fernando', 30));
-console.log(mostraCidade('Fernando', 'Itaquaquecetuba'));
-console.log(mostraHobby('Fernando', 'Futebol'));
